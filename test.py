f90fa10b808993106bb77d82083077fbbf446a7d
Suraj Lambor <163487079+suraj169Pi@users.noreply.github.com>
2025-11-28 18:34:37 +0530

    Fix #3: Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,15 @@
-
 import re
 
 def removeDuplicateWords(input):
-
-    regex = r'\b(\w)(?:\W+\1\b)+'
-
+    # Modified regex to match whole words and their consecutive duplicates
+    regex = r'\b(\w+)\s+\1\b'
     return re.sub(regex, r'\1', input, flags=re.IGNORECASE)
 
 str1 = "Good bye bye world world"
 print(removeDuplicateWords(str1))
 
-
 str2 = "Ram went went to to his home"
 print(removeDuplicateWords(str2))
 
-
 str3 = "Hello hello world world"
 print(removeDuplicateWords(str3))
-
-
